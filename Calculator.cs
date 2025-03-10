51adf9ca3a34fa547730a79404c87464b1e6e9c2
olexijklimov <klim.leha.36@gmail.com>
2025-03-10 12:51:43 +0200

    Добавьте файлы проекта.

---
+++ Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public static class Calculator
+    {
+        public static int[] Add(int[] f1, int[] f2)
+        {
+            int[] result = new int[2];
+            result[0] = f1[0] * f2[1] + f2[0] * f1[1];
+            result[1] = f1[1] * f2[1];
+            return result;
+        }
+
+        public static int[] Subtract(int[] f1, int[] f2)
+        {
+            int[] result = new int[2];
+            result[0] = f1[0] * f2[1] - f2[0] * f1[1];
+            result[1] = f1[1] * f2[1];
+            return result;
+        }
+
+        public static int[] Multiply(int[] f1, int[] f2)
+        {
+            int[] result = new int[2];
+            result[0] = f1[0] * f2[0];
+            result[1] = f1[1] * f2[1];
+            return result;
+        }
+
+        public static int[] Divide(int[] f1, int[] f2)
+        {
+            int[] result = new int[2];
+            result[0] = f1[0] * f2[1];
+            result[1] = f1[1] * f2[0];
+            return result;
+        }
+    }
+}
---
+++ xUnitTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace Calculator
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Сheck_Add()
+        {
+            //Arrange
+            int[] f1 = { 1, 2 };
+            int[] f2 = { 2, 2 };
+
+            //Act
+            int[] f = Calculator.Add(f1, f2);
+
+            //Assert
+            Assert.Equal(6, f[0]);
+            Assert.Equal(4, f[1]);
+        }
+    }
+}
